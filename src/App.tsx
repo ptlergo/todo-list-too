24bfcaadb7dbce74a542400c41689554c9711b0c
ptlergo <ptlergo@gmail.com>
2019-02-14 19:36:57 -0500

    deletetodo and passing actions as an object to todo component

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,50 @@ export interface ITodoProps {
   key: number;
   index: number;
   todo: any;
+  actions: { completeTodo(index: number): any; deleteTodo(index: number): any };
 }
 
 export default () => {
   const [todos, setTodos] = useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "build really cool todo app" }
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet friend for lunch", isCompleted: false },
+    { text: "build really cool todo app", isCompleted: false }
   ]);
-
 
   const addTodo = (text: any): void => {
     // add the texts to the todos array
-    const newTodos = [...todos, {text}]
+    const newTodos: any = [...todos, { text }];
     // set the new array to the todos state
-    setTodos(newTodos)
-  }
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    // list of todos
+    const newTodos: any = [...todos];
+    // specific todo iscompleted based on index. change value
+    newTodos[index].isCompleted = true;
+    // save changes
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index: number): void => {
+    const newTodos: any = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
   return (
     <div>
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            actions={{ completeTodo, deleteTodo }}
+          />
         ))}
       </div>
-      <TodoForm
-      addTodo={addTodo} />
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 };
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { ITodoProps } from "../App";
 
 export default (props: ITodoProps): JSX.Element => {
-    const {todo} = props;
-  return <div className="todo">{todo.text}</div>;
+  const { todo, index, actions } = props;
+  const styleProps = { textDecoration: todo.isCompleted ? "line-through" : "" };
+  return (
+    <div className="todo" style={styleProps}>
+      {todo.text}
+      <div>
+        <button onClick={() => actions.completeTodo(index)}>Complete</button>
+        <button onClick={() => actions.deleteTodo(index)}>X</button>
+      </div>
+    </div>
+  );
 };
